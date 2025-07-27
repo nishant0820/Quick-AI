dbda10c29f8d4619db3126aa43c9a02ed4094e0a
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-07-27 08:57:08 +0530

    completed blogtitles page

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,4 +1,4 @@
-import { Edit, Sparkles } from 'lucide-react'
+import { Hash, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
 
 const BlogTitles = () => {
@@ -19,31 +19,31 @@ const BlogTitles = () => {
           <Sparkles className='w-6 text-[#8E37EB]' />
           <h1 className="text-xl font-semibold">AI Title Generator</h1>
         </div>
-        <p className="mt-6 text-sm font-medium">Article Topic</p>
+        <p className="mt-6 text-sm font-medium">Keyword</p>
         <input onChange={(e) => setInput(e.target.value)} value={input} type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of Artificial Intelligence is...' required />
-        <p className="mt-4 text-sm font-medium">Article Length</p>
+        <p className="mt-4 text-sm font-medium">Category</p>
         <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
-          {blogCategories.map((item, index) => (
-            <span onClick={() => setSelectedCategory(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedCategory.text === item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300'}`} key={index}>{item.text}</span>
+          {blogCategories.map((item) => (
+            <span onClick={() => setSelectedCategory(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedCategory === item ? 'bg-purple-50 text-purple-700' : 'text-gray-500 border-gray-300'}`} key={item}>{item}</span>
           ))}
         </div>
         <br />
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
-          <Edit className='w-5' />
-          Generate Article
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Hash className='w-5' />
+          Generate Title
         </button>
       </form>
 
 
-      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
         <div className="flex items-center gap-3">
-          <Edit className='w-5 h-5 text-[#4A7AFF]' />
-          <h1 className="text-xl font-semibold">Generated Article</h1>
+          <Hash className='w-5 h-5 text-[#8E37EB]' />
+          <h1 className="text-xl font-semibold">Generated Title</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Edit className='w-9 h-9' />
-            <p className="">Enter a topic and click "Generate Article" to get started</p>
+            <Hash className='w-9 h-9' />
+            <p className="">Enter a topic and click "Generate Title" to get started</p>
           </div>
         </div>
       </div>
