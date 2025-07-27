78786ce0168214898640bc0ae50ae97b6af0e41c
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-07-27 21:51:34 +0530

    created backend and connected database

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => res.send('Server is LIVE...'))
+
+app.use(requireAuth())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+})
