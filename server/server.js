6edef25f53a275a7c9aa3197079b93a045af3988
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-07-28 23:35:59 +0530

    create api for generate blog title and images

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ import cors from 'cors';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import 'dotenv/config'
 import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app = express()
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
