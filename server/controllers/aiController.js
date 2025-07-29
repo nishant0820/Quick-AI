4c8e7fe1c06b04bcbe0ebbceccdf45a6bdcf8b4c
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-07-30 00:43:23 +0530

    Add api for removeImageBackground and removeImageObject

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -106,9 +106,9 @@ export const generateImage = async (req, res) => {
             return res.json({ success: false, message: "This feature is only available for premium subscriptions." })
         }
 
-        const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?width=1024&height=1024&nologo=true`
+        const Image = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?width=1024&height=1024&nologo=true`
         
-        const response = await axios.get(imageUrl, {
+        const response = await axios.get(Image, {
             responseType: 'arraybuffer'
         })
         
@@ -119,6 +119,64 @@ export const generateImage = async (req, res) => {
         await sql `INSERT INTO creations (user_id, prompt, content, type, publish) VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`
 
         res.json({ success: true, content: secure_url });
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+        
+    }
+}
+
+export const removeImageBackground = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const { image } = req.file
+        const plan = req.plan
+
+        if(plan !== 'premium'){
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions." })
+        }
+        const {secure_url} = await cloudinary.uploader.upload(image.path, {
+            transformation: [
+                {
+                    effect: "background_removal",
+                    background_removal: "remove_the_background"
+                }
+            ]
+        })
+
+        await sql `INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Remove background from image', ${secure_url}, 'image')`
+
+        res.json({ success: true, content: secure_url });
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+        
+    }
+}
+
+export const removeImageObject = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const {object} = req.body
+        const { image } = req.file
+        const plan = req.plan
+
+
+        if(plan !== 'premium'){
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions." })
+        }
+        const {public_id} = await cloudinary.uploader.upload(image.path)
+        
+        const imageUrl = cloudinary.url(public_id, {
+            transformation: [{effect: `gen_remove:${object}`}],
+            resource_type: 'image'
+        })
+
+        await sql `INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`
+
+        res.json({ success: true, content: imageUrl });
 
     } catch (error) {
         console.log(error.message);
